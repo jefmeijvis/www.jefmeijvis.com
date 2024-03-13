5bf7be8b7a405f3d99ae6db1538f175af7da2013
Jef Meijvis <jef.meijvis@hotmail.com>
2024-03-13 16:59:58 +0100

    Chunking

--- vite.config.ts
+++ vite.config.ts
@@ -14,5 +14,20 @@ export default defineConfig({
 		sveltekit(),		
 		VitePluginRestart(vitePluginRestartOptions),
 		viteStaticCopy(viteStaticCopyOptions)
-	]
+	],
+
+	// https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
+	build: {
+        rollupOptions: {
+            output:{
+                manualChunks(id) {
+					if (id.includes('node_modules')) {
+                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
+                    }
+                }
+            }
+        },
+		chunkSizeWarningLimit : 10_000
+
+    }
 });
