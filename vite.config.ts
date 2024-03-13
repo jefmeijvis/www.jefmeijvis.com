96d296c539450e5c110880e0391612e722dd908f
Jef Meijvis <jef.meijvis@hotmail.com>
2024-03-13 19:48:38 +0100

    Removed chunk splitting

--- vite.config.ts
+++ vite.config.ts
@@ -18,15 +18,6 @@ export default defineConfig({
 
 	// https://stackoverflow.com/questions/69260715/skipping-larger-chunks-while-running-npm-run-build
 	build: {
-        rollupOptions: {
-            output:{
-                manualChunks(id) {
-					if (id.includes('node_modules')) {
-                        return id.toString().split('node_modules/')[1].split('/')[0].toString();
-                    }
-                }
-            }
-        },
 		chunkSizeWarningLimit : 10_000
 
     }
