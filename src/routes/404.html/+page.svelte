52f40d33daa24b7ecf583ae08fa901d991561e3b
Jef Meijvis <jef.meijvis@hotmail.com>
2024-03-15 15:10:42 +0100

    404 page test

---
+++ src/routes/404.html/+page.svelte
@@ -0,0 +1,11 @@
+<script lang="ts">
+    import Notfound from "$lib/components/notfound.svelte";
+
+    /*
+        Via: https://vercel.com/guides/custom-404-page:
+        Emit a 404.html file to your Output Directory and it 
+        will be served as the 404 page when a route does not match any other static file.
+    */
+</script>
+
+<Notfound></Notfound>
