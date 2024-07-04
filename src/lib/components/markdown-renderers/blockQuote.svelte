19e30a621dfca8b1231734d9b880ac465c1f60ec
Jef Meijvis <jef.meijvis@hotmail.com>
2024-07-04 11:25:29 +0200

    Applied correct styling and theming to the blockquote markdown component

--- src/lib/components/markdown-renderers/blockQuote.svelte
+++ src/lib/components/markdown-renderers/blockQuote.svelte
@@ -1,19 +1,39 @@
-<blockquote>
+<script>
+
+    import { theme } from "../../../stores";
+
+</script>
+<blockquote class:light={$theme == 'light'} class:dark={$theme != 'light'}>
     <p><b>Note</b></p>
     <slot></slot>
 </blockquote>
 
 
 <style>
+    .light
+    {
+        background-color: rgb(255, 208, 0);
+        outline: 1px rgb(189, 159, 7) solid;
+    }
+
+    .dark
+    {
+        background-color: rgb(95, 83, 18);
+        outline: 1px rgb(185, 155, 4) solid;
+    }
+
+    .dark p
+    {
+        color:rgb(255, 213, 0);
+    }
+
     blockquote
     {
         margin-top: 1rem;
         margin-bottom: 1rem;
         padding : 1rem;
         border-radius: .5rem;
-        background-color: var(--color-note-background);
         text-align: justify;
         font-weight: 300;
-        border : .1rem solid var(--color-note-border)
     }    
 </style>
