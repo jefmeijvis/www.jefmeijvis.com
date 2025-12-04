6e59bda5a9bf60c51a8e89ddb6203b704cdf3dce
Jef Meijvis <jef.meijvis@hotmail.com>
2025-12-04 21:51:42 +0100

    Create theming constants

---
+++ src/constants.ts
@@ -0,0 +1,5 @@
+export class Themes 
+{
+    public static Light = "light"
+    public static Dark = "dark"
+}
--- src/stores.ts
+++ src/stores.ts
@@ -1,8 +1,9 @@
 // src/lib/stores.ts
 import { writable } from "svelte/store"
 import { browser } from "$app/environment"
+import { Themes } from "./constants"
 
-export const theme = writable(browser && localStorage.getItem("theme") || "light")
+export const theme = writable(browser && localStorage.getItem("theme") || Themes.Light)
 theme.subscribe((val) => {
   if (browser) return (localStorage.theme = val)
 })
